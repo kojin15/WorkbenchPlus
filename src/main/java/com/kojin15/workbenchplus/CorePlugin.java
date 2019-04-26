c09b18669e6e60ada696ea75e35e88003f6f9687
どらごん <kojin15@amigo2.ne.jp>
2019-04-26 19:01:23 +0900

    Fix problems that were not recognized as mod

---
+++ src/main/java/com/kojin15/workbenchplus/CorePlugin.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 kojin15
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kojin15.workbenchplus;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.Map;
+
+/**
+ * @author kojin15.
+ */
+public class CorePlugin implements IFMLLoadingPlugin {
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return "com.kojin15.workbenchplus.SetupClass";
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
